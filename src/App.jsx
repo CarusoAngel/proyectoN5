7e3e2992791cb67aca14d6befce7bf1f01af87e2
CarusoAngel <acarusoangelilli@gmail.com>
2025-03-07 01:01:40 -0300

    Corrección en DataPage.jsx y mejoras en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import DataPage from './pages/DataPage';
+import DataPage from './pages/DataPage.jsx';
 
 function App () {
   return (
--- src/pages/DataPage.jsx
+++ src/pages/DataPage.jsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function DataPage () {
+    
+    const [launches, setLaunches] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect (() => {
+        fetch('https://api.spacexdata.com/v4/launches/upcoming')
+        .then((response) => response.json())
+        .then((data) => {
+            setLaunches(data.slice(0, 10));
+            setLoading(false);
+        })
+        .catch((error) => console.error('Error al obtener los datos:', error));
+    }, []);
+
     return (
         <div className="container mt-5">
-            <h1 className="text-center text-warning">Página de datos</h1>
-            <p className="text-center">Mostrar info de la API</p>
+            <h1 className="text-center text-primary">Próximos lanzamientos de SpaceX</h1>
+            {loading ?(
+                <p className="text-center">Cargando lanzamientos...</p>
+            ):(
+                <div className="row">
+                    {launches.map((launch) => (
+                        <div key={launch.id} className="col-md-6 mb-4">
+                            <div className="card p-3 shadow">
+                                <h5 className="fw-bold">{launch.name}</h5>
+                                <p><strong>Fecha:</strong> {new date(launch.date_utc).toLocalDatestring()}</p>
+                                <p><strong>Detalles:</strong> { launch.details ? launch.details : "Sin información"}</p>
+                                {launch.links.patch.small && (
+                                    <img src={launch.links.patch.small} alt="Misión" className="img-fluid" />
+                                )}
+                            </div>
+                        </div>
+                    ))}
+            </div>
+            )}
         </div>
     );
 }
