35051f5aaa6a0a37adbf685d66ab744ba2b5ce11
CarusoAngel <acarusoangelilli@gmail.com>
2025-03-09 21:44:23 -0300

    Corrigiendo importación de Home.jsx para despliegue

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from "./components/Navbar";
-import Home from './pages/Home';
+import Home from './pages/Home.jsx';
 import DataPage from './pages/DataPage';
 import "./App.css";
 
